9d637aab54e4b369ca3b5e8e5ecf65febd8ea83f
Nicolò Belcastro <nicolo.belcastro@telepass.com>
2024-05-22 16:51:40 +0200

    WIP

---
+++ src/main/java/com/nick/lesson_1/MoltiplicaDueInteri.java
@@ -0,0 +1,18 @@
+package com.nick.lesson_1;
+
+import java.util.Scanner;
+
+public class MoltiplicaDueInteri {
+    public static void main(String args[]) {
+        System.out.println("Ciao, so moltiplicare due numeri interi.");
+        Scanner keyboardScanner = new Scanner(System.in);
+
+        System.out.println("Scrivi il primo valore e premi invio");
+        int input1 = keyboardScanner.nextInt();
+
+        System.out.println("Scrivi il secondo valore e premi invio");
+        int input2 = keyboardScanner.nextInt();
+
+        System.out.println("Il risultato è: " + input1 * input2);
+    }
+}

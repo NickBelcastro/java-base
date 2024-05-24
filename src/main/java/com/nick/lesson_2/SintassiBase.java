25c870ebfada17ed3bafcffb381f70f3f4d76c38
Nicolò Belcastro <nicolo.belcastro@telepass.com>
2024-05-24 12:44:56 +0200

    Add SIntassiBase

---
+++ src/main/java/com/nick/lesson_2/SintassiBase.java
@@ -0,0 +1,13 @@
+package com.nick.lesson_2;
+
+public class SintassiBase {
+    // dichiarazione metodo main
+    public static void main(String[] args) {
+        // dichiarazione e inizializzazione di variabili
+        int variabile1 = 10;
+        int variabile2 = 20;
+
+        // Stampa della somma tra le due variabili
+        System.out.println(variabile1 + variabile2);
+    }
+}
